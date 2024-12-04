56926719fb2ddeb5aa63065125b70f1d40adb06a
40718426 <sandymadill2000@gmail.com>
2024-12-04 18:02:18 +0000

    "integrated population reports into web app"

--- src/main/java/com/napier/semga/Population.java
+++ src/main/java/com/napier/semga/Population.java
@@ -1,10 +1,8 @@
 package com.napier.semga;
 
 public class Population {
-    public int cityPopulation;
-    public int nonCityPopulation;
-    public String country;
-    public String regions;
-    public String continents;
-    public int totalPopulation;
+    public long cityPopulation;
+    public long nonCityPopulation;
+    public String name;
+    public long totalPopulation;
 }
